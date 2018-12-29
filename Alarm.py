1a0785c20d3ebbb7349b1bfa46302fd309a4afa5
Cooper <c@ooper.space>
2018-12-29 18:57:43 -0400

    Added Quote

--- Alarm.py
+++ Alarm.py
@@ -13,6 +13,9 @@ from gtts import gTTS
 from dateutil.parser import parse
 #Weather
 from weather import Weather, Unit
+#Quotes
+import requests
+import json
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
@@ -35,12 +38,24 @@ def main():
                                         orderBy='startTime').execute()
     events = events_result.get('items', [])
 
+    #Quote
+    url = 'https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en'
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+    result = requests.get(url, headers=headers)
+    json_str = result.content.decode('utf-8')
+    content = json.loads(json_str)
+    quote = "Like the great have said, " + content["quoteText"]
+    print(quote);
+
+    #Date
     day = datetime.strftime(datetime.now(), "%A %B %d")
     print(day + '\nSchedule:')
-    weather = Weather(unit=Unit.CELSIUS)
 
+    #Weather
+    weather = Weather(unit=Unit.CELSIUS)
     lookup = weather.lookup(4177)
     condition = lookup.condition
+
     line = "Good Morning Cooper! Today is" + day + ". I looks like its going to be " + condition.text + " today."
 
     if not events:
@@ -52,9 +67,11 @@ def main():
             start = event['start'].get('dateTime', event['start'].get('date'))
             dt = parse(start)
             print(dt.strftime("%I:%M %p"), event['summary'])
-            #play Audio
             line += "At " + dt.strftime("%I:%M %p") + " you will, " + event['summary']+ "."
-    line += " I hope you have a lovely day"
+
+    line += quote
+
+    #play Audio
     tts = gTTS(text=line, lang='en')
     tts.save("reply.mp3")
     os.system("ffplay -loglevel -8 -autoexit reply.mp3")
